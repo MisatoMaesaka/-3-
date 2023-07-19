bc193cd2a55a1e8a028e41bd335e4dcc49e6564b
Misato-Maesaka <r012mbmaesaka@gmail.com>
2023-07-19 23:01:37 +0900

    refactor FruitsMapのsetFruitsMapを、getFruitsMapのメソッド名に変更

--- src/FruitsMap.java
+++ src/FruitsMap.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FruitsMap {
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitsMap {
+    public static Map<String, String> getFruitsMap() {
+
+        Map map = new HashMap();
+
+        map.put("apple", "リンゴ");
+        map.put("banana", "バナナ");
+        map.put("orange", "オレンジ");
+
+        return map;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int count = 5;
+        Map<String, String> map = FruitsMap.getFruitsMap();
+
+        // Mapからデータを取得
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+        for (String key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key));
+        }
+
+        // Listを宣言し、valuesメソッドを使用してMapの値を取得する
+        List<String> list = new ArrayList<>(map.values());
+
+        for (int i = 0; i <= count; i++) {
+            try {
+                // 要素を指定してListの値を取得
+                System.out.println(list.get(i));
+            } catch (Exception e) {
+                System.out.println("例外処理になります。");
+            }
+        }
+    }
 }
+
+
+
